16309bba6787ad2ad5b1e7b89da10796e4bbc745
전병길 <jeonbg@kakao.com>
2025-04-28 19:54:11 +0900

    style: shadcn 컴포넌트 추가

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -6,6 +6,8 @@ const config = {
   darkMode: ['class'],
   content: [
     './pages/**/*.{ts,tsx}',
+    './widgets/**/*.{ts,tsx}',
+    './features/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
